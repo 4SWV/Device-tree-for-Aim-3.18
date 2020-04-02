571a212443030be98b85be2eb923f245ebff2122
SunnyRaj84348 <SunnyRaj84348@users.noreply.github.com>
2020-04-02 15:52:23 +0000

    mido: Tweaks For Better Ram Management
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- prop.mk
+++ prop.mk
@@ -215,7 +215,15 @@ persist.dirac.acs.ignore_error=1 \
 ro.audio.soundfx.dirac=true \
 persist.audio.dirac.speaker=true
 
-# Spectrum
+# Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    spectrum.support=1 \
-    persist.spectrum.kernel=Parallax
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
+
